38dde67b8443e3d5c356db632b85c3a050c69b8e
Brett Schneider <pecac27@gamil.com>
2019-10-30 21:12:56 -0400

    test

--- Weather_Tweets.py
+++ Weather_Tweets.py
@@ -13,6 +13,7 @@ import requests as req
 import datetime
 import sys
 import openweathermapy.core as owm
+import os
 consumer_key = os.environ.get("consumer_key")
 consumer_secret = os.environ.get("consumer_secret")
 access_token = os.environ.get("access_token")
