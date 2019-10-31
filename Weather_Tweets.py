1251e08ba711f0d6e695f133995abc3f11ab0195
Brett Schneider <bschneider@schgroup.com>
2019-10-30 20:39:35 -0400

    test

---
+++ Weather_Tweets.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+# Dependencies
+import tweepy
+import time
+import json
+import random
+import requests as req
+import datetime
+import sys
+import openweathermapy.core as owm
+sys.path.append("../../../../../../../..")
+from config import consumer_key, consumer_secret, access_token, access_token_secret
+from weatherconfig import api_key
+
+
+# In[5]:
+
+
+# Twitter API Keys
+consumer_key = consumer_key
+consumer_secret = consumer_secret
+access_token = access_token
+access_token_secret = access_token_secret
+
+
+# In[7]:
+
+
+# Weather API Key
+weather_api_key = api_key
+
+
+# In[ ]:
+
+
+# Create a function that gets the weather in London and Tweets it
+def WeatherTweet(tweet):
+
+    # Construct a Query URL for the OpenWeatherMap
+    url = "http://api.openweathermap.org/data/2.5/weather?"
+    city = "Washington, D.C."
+    units = "imperial"
+    query_url = url + "appid=" + api_key + "&q=" + city + "&units=" + units
+    settings = {"units": "imperial", "appid": weather_api_key}
+    # Perform the API call to get the weather
+    washington = owm.get_current(city, **settings)
+    washingtonweather = washington["main"]["temp"]
+    # Twitter credentials
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
+
+    # Tweet the weather
+    currenttime = datetime.datetime.now().strftime("%I:%M %p")
+    api.update_status(f"The weather in DC RIGHT NOW is {washingtonweather}F timestamp: {currenttime}")
+
+    # Print success message
+    print("This was successful")
+
+
+# In[ ]:
+
+
+# Set timer to run every 1 hour
+# Create a loop that calls the TweetOut function every minute
+counter = 0
+
+# Infinite loop
+while(True):
+
+    # Call the TweetQuotes function and specify the tweet number
+    WeatherTweet(counter)
+
+    # Once tweeted, wait 60 seconds before doing anything else
+    time.sleep(3600)
+
+    # Add 1 to the counter prior to re-running the loop
+    counter = counter + 1
+
+
+# In[34]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
